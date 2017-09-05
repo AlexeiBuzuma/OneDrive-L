2262078ef0f386ed3416d65146aed766e8b0990d
Aliaksei Buziuma <Aliaksei_Buziuma@epam.com>
2017-09-05 09:15:50 +0300

    Added auth functionality for OneDrive for personal use

---
+++ onedrive_service/src/onedrive_service/authentication/base_client.py
@@ -0,0 +1,18 @@
+import os
+import onedrivesdk
+
+
+class BaseOneDriveClient(onedrivesdk.OneDriveClient):
+    """ Base class for OneDrive API clients.
+    """
+
+    @staticmethod
+    def check_saved_session_exists() -> bool:
+        """ Returns 'True' if saved sessions exists.
+        """
+
+        # TODO: to be implemented.
+
+        current_path = os.getcwd()
+        session_path = os.path.join(current_path, 'session.pickle')
+        return os.path.exists(session_path)
